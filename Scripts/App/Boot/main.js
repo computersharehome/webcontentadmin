b9c14d3073e5737f7f43e053218e92bd1dd947a8
Jim Xu <computersharehome@gmail.com>
2016-12-30 21:22:08 -0500

    remove Scripts folder

--- Scripts/App/Boot/main.js
+++
@@ -1,8 +0,0 @@
-﻿require(['router', "app", "logger", "jqx-all", "mockjax", "bootstrap", "ko.bindings", "date"], function (router, app) {
-	
-    $("#document").ready(function () {
-	    app.start();
-	    router.startRouting();  
-		app.logger.info("app started");
-    });
-});
--- Scripts/App/Boot/menu.js
+++
@@ -1,1 +0,0 @@
-[{"id": "#/Home/", "text": "Home", "parentid": "-1", "subMenuWidth": "250px" }, {"id": "#/Portfolio/", "text": "Portfolio", "parentid": "-1", "subMenuWidth": "250px" }, {"id": "#/Settings/", "text": "Settings", "parentid": "-1", "subMenuWidth": "250px" }]
